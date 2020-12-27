17642e8d224412eef825a69d11cea9240f39f9da
unknown <ioana.iris@gmail.com>
2020-12-27 17:16:27 +0200

    made the bee follow adonis, tried to animate adonis but failed miserably

---
+++ BeeFobia/Assets/floatTheBee.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floatTheBee : MonoBehaviour {
+	bool floatup;
+
+	// Use this for initialization
+	void Start () {
+		floatup = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (floatup)
+			StartCoroutine(floatingup());
+		else if (!floatup)
+			StartCoroutine(floatingdown());
+	}
+
+	IEnumerator floatingup()
+	{
+		var new_vec = new Vector3(transform.position.x, transform.position.y+0.6f * Time.deltaTime, transform.position.z);
+		transform.position = new_vec;
+		yield return new WaitForSeconds(1);
+		floatup = false;
+	}
+	IEnumerator floatingdown()
+	{
+		var new_vec = new Vector3(transform.position.x, transform.position.y - 0.6f * Time.deltaTime, transform.position.z);
+		transform.position = new_vec;
+		yield return new WaitForSeconds(1);
+		floatup = true;
+	}
+}
