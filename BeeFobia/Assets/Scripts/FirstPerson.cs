823a3d80ea2ee3323374bb2a91a32a65a571e150
Diana <ancamariadiana49@gmail.com>
2020-12-28 14:46:36 +0200

    changed model, added animations for her, changed unity version

---
+++ BeeFobia/Assets/Scripts/FirstPerson.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPerson : MonoBehaviour
+{
+	public Rigidbody Rigid;
+	float MouseSensitivity = 1f;
+	float MoveSpeed = 0.02f;
+	float JumpForce = 2f;
+	Animator animator;
+
+	// Use this for initialization
+	void Start()
+	{
+		Rigid = this.GetComponent<Rigidbody>();
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		bool forward = Input.GetKey(KeyCode.UpArrow);
+		bool isWalking = animator.GetBool("isWalking");
+		//animation trggers movement
+		if (!isWalking && forward)
+			animator.SetBool("isWalking", true);
+		if (isWalking && !forward)
+			animator.SetBool("isWalking", false);
+
+		//actual movement + rotations
+		Rigid.MoveRotation(Rigid.rotation * Quaternion.Euler(new Vector3(0, Input.GetAxis("Mouse X") * MouseSensitivity, 0)));
+		Rigid.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * MoveSpeed) + (transform.right * Input.GetAxis("Horizontal") * MoveSpeed));
+		if (Input.GetKeyDown("space"))
+			Rigid.AddForce(transform.up * JumpForce);
+	}
+
+}
---
+++ BeeFobia/Assets/Scripts/beeLookAt.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beeLookAt : MonoBehaviour
+{
+
+
+	public Transform target;
+	public GameObject target_for_movement;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+
+		//transform.LookAt(target);
+		transform.rotation = Quaternion.LookRotation(target.position, Vector3.up);
+		transform.Rotate(0.0f, 90.0f, 0.0f);
+		var new_vec = new Vector3(target_for_movement.transform.position.x, transform.position.y, target_for_movement.transform.position.z);
+		transform.position = Vector3.MoveTowards(transform.position, new_vec, 0.07f);
+		//transform.position = target.position + new Vector3(3f, transform.position.y - target.position.y, 3f);
+
+	}
+}
